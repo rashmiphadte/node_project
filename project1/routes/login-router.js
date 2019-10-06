879569a3c855774d4af5f2a2f77aa05b0192c432
rashmiphadte <rashmiphadte@gmail.com>
2019-10-06 13:28:32 +0530

    created test post route

--- project1/routes/login-router.js
+++ project1/routes/login-router.js
@@ -8,5 +8,8 @@ router.get('/', function(req, res) {
   console.log("hello")
     return res.send("hello");
 });
+router.post('/', function(req, res) {
+    return res.send("this is post call");
+});
 
 module.exports = router;
